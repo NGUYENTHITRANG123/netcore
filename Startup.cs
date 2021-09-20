57bdf9757d819bb566db273e05229dab1b59edf5
NGUYENTHITRANG123 <nguyenthitrangkbn@gmail.com>
2021-09-20 23:37:36 +0700

    Commit_123

--- Startup.cs
+++ Startup.cs
@@ -12,23 +12,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace demoMVC
 {
-    public class Startup
+  public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
-        {
-             Environment = env;
-            Configuration = configuration;
-        }
+        Environment = env;
+        Configuration = configuration;
+    }
 
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
 
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-            services.AddDbContext<MvcMovieContext>(options =>
+        services.AddDbContext<MvcMovieContext>(options =>
         {
             var connectionString = Configuration.GetConnectionString("MvcMovieContext");
 
@@ -41,34 +40,32 @@ namespace demoMVC
                 options.UseSqlServer(connectionString);
             }
         });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
     }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
 }
